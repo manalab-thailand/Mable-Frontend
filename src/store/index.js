a24d8cb006de6cfc03aca136976a2d39c906b6b9
Arnont <nongnont.170442@gmail.com>
2021-02-12 23:07:41 +0700

    test

--- src/store/index.js
+++ src/store/index.js
@@ -14,16 +14,16 @@ Vue.use(Vuex)
  * with the Store instance.
  */
 
-export default function (/* { ssrContext } */) {
-  const Store = new Vuex.Store({
-    modules: {
-      // example
-    },
+export default function( /* { ssrContext } */ ) {
+    const Store = new Vuex.Store({
+        modules: {
+            // example
+        },
 
-    // enable strict mode (adds overhead!)
-    // for dev mode only
-    strict: process.env.DEBUGGING
-  })
+        // enable strict mode (adds overhead!)
+        // for dev mode only
+        strict: process.env.DEBUGGING
+    })
 
-  return Store
+    return Store
 }
