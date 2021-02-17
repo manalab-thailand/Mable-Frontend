cda9fa820b18d1e8b5aafd947cba3902e3c53512
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-02-17 17:03:32 +0700

    test commit

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,11 +1,36 @@
 <template>
-    <q-page class="flex flex-center">
-    <p>this is the placement for map bro</p>
-  </q-page>
+  <div class="q-pa-md">
+    <q-btn-dropdown color="primary" label="Select Floor">
+      <q-list>
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Floor 1</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Floor 2</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Floor 3</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+  </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    onItemClick () {
+      // console.log('Clicked on an Item')
+    }
+  }
 }
 </script>
 
