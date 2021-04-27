58d693beada7e59654b2c128ebdbcbb1afbee077
supanat2542 <62238322+supanat2542@users.noreply.github.com>
2021-04-27 08:43:24 +0700

    scan log time edit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,13 +5,13 @@
       subTitle="ศูนย์กลางการควบคุม"
     ></section-header>
     <div class="row q-gutter-col-md">
-      <div v-for="items in list2" class="col-2" :key="items.taguse_id">
-        <add-card v-if="items.visitor_id == 0" :id="items.taguse_id"></add-card>
+      <div v-for="item1 in list5" class="col-2" :key="item1.tag_id">
         <div v-for="item in dashbord" :key="item.id">
-          <device-card
+          <div v-if="item1.tag_address == item.tag_address">
+            <div v-if="item.visitor_id!=0">
+               <device-card
             class="col-4"
-            v-if="item.id == items.taguse_id"
-            :id="items.taguse_id"
+            :id="item1.tag_id"
             :fname="item.fname"
             :lname="item.lname"
             :tel="item.tel"
@@ -19,6 +19,11 @@
             :category="item.category"
             :location="item.location"
           ></device-card>
+            </div>
+            <div v-else>
+              <add-card :id="item1.tag_id"></add-card>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -46,6 +51,7 @@ export default {
       list2: undefined,
       list3: undefined,
       list4: undefined,
+      list5: undefined,
     };
   },
   async mounted() {
@@ -69,40 +75,56 @@ export default {
     console.warn("list4 scanerlog");
     console.warn(this.list4);
 
-    console.log(this.list[0].first_name);
-    for (var i = 0; i < this.list2.length; i++) {
-      for (var j = 0; j < this.list.length; j++) {
-        if (this.list2[i].visitor_id == this.list[j].visitor_id) {
-          // console.warn(
-          //   "I : " + this.list2[i].taguse_id + " j :" + this.list[j].visitor_id
-          // );
-          // console.warn("id : " + this.list2[i].taguse_id);
-          // console.warn(
-          //   "Name " + this.list[j].first_name + " " + this.list[j].last_name + i
-          // );
-          // console.warn("tel : " + this.list[j].tel);
-          // console.warn("category : " + this.list[j].category);
-          // console.warn("contract : " + this.list[j].contract);
-          // console.warn("id_civiliz : " + this.list[j].id_civiliz);
-          // console.warn("tag_address : " + this.list2[i].tag_address);
-          // console.warn("tag_address(log) : " + this.list4[i].device_address);
-          // console.warn("scanner_id(log) : " + this.list4[i].scanner_id);
+    let resp5 = await axios.get("http://localhost:3030/api/tags");
+    this.list5 = resp5.data.result.rows;
+    console.warn("list5 tags");
+    console.warn(this.list5);
+
+    for (var i = 0; i < this.list5.length; i++) {
+      for (var j = 0; j < this.list2.length; j++) {
+        if (
+          this.list5[i].tag_address == this.list2[j].tag_address &&
+          this.list2[j].time_stop == null
+        ) {
+          console.warn("kan");
+          console.warn(this.list2[j].taguse_id);
           for (var k = 0; k < this.list3.length; k++) {
             if (this.list4[i].scanner_id == this.list3[k].scanner_address) {
               // console.warn("room : " + this.list3[k].room);
               const newItem = {
                 id: this.list2[i].taguse_id,
-                fname:this.list[j].first_name,
-                lname:this.list[j].last_name,
-                tel:this.list[j].tel,
-                contract:this.list[j].contract,
-                category:this.list[j].category,
+                fname: this.list[j].first_name,
+                lname: this.list[j].last_name,
+                tel: this.list[j].tel,
+                contract: this.list[j].contract,
+                category: this.list[j].category,
                 location: this.list3[k].room,
+                tag_address: this.list5[i].tag_address,
                 visitor_id: this.list[j].visitor_id,
               };
               this.dashbord.push(newItem);
             }
           }
+          break;
+        } else if (
+          this.list5[i].tag_address == this.list2[j].tag_address &&
+          this.list2[j].time_stop != null
+        ) {
+          console.warn("kan 1");
+          console.warn(this.list2[j].taguse_id);
+          const newItem2 = {
+            id: this.list2[i].taguse_id,
+            fname: "",
+            lname: "",
+            tel: "",
+            contract: "",
+            category: "",
+            location: "",
+            tag_address: this.list5[i].tag_address,
+            visitor_id: 0,
+          };
+          this.dashbord.push(newItem2);
+          break;
         }
       }
     }
--- src/pages/TimelineTable.vue
+++ src/pages/TimelineTable.vue
@@ -5,8 +5,12 @@
       subTitle="แสดงข้อมูลการใช้งานของ Users"
     ></section-header>
     <q-tabs v-model="tab" class="q-mb-lg">
-      <q-tab class="text-purple" name="Dairy" label="Dairy" />
-      <q-tab class="text-orange" name="Monthly" label="Monthly" />
+      <q-tab class="text-purple" name="Dairy" label="Dairy"  />
+      <q-tab
+        class="text-orange"
+        name="Monthly"
+        label="Monthly"
+      />
       <q-tab class="text-teal" name="Select Date" label="Select Date" />
     </q-tabs>
 
@@ -21,7 +25,7 @@
       <q-tab-panel name="Dairy">
         <div class="q-pa-md">
           <q-table
-            title="Timeline - Dairy 18/03/2021"
+            title="Timeline - Dairy 13/01/2021"
             :data="list_day"
             :columns="columns"
             row-key="name"
@@ -59,8 +63,8 @@
       <q-tab-panel name="Monthly">
         <div class="q-pa-md">
           <q-table
-            title="Timeline - Monthry 03/2021"
-            :data="list2"
+            title="Timeline - Monthry 02/2021"
+            :data="list_month"
             :columns="columns"
             row-key="name"
             class="rounded-borders-10 shadow-5"
@@ -81,8 +85,7 @@
                     size="md"
                     color="primary-gradient"
                     round
-                    to="/timeline"
-                    :id="1"
+                    :to="'/timeline/' + props.row.id"
                     :icon="props.expand ? '' : 'person_pin_circle'"
                   />
                 </q-td>
@@ -97,10 +100,6 @@
       <!-- ================================================================================================================================= -->
       <q-tab-panel name="Select Date">
         <div class="row">
-          <!-- <div class="row">Enter Name</div>
-          <div class="q-pa-md" style="width: 350px">
-            <q-input v-model="text"/>
-          </div> -->
           <div class="row-4">Select Date</div>
           <div class="q-pa-md" style="max-width: 300px">
             <q-input filled v-model="date" mask="date" :rules="['date']">
@@ -117,6 +116,7 @@
                           v-close-popup
                           label="Close"
                           color="primary"
+                          @click="select()"
                           flat
                         />
                       </div>
@@ -130,8 +130,8 @@
 
         <div class="q-pa-md">
           <q-table
-            title="Timeline - Date 18/03/2021"
-            :data="list2"
+            title="Timeline"
+            :data="list_select"
             :columns="columns"
             row-key="name"
             class="rounded-borders-10 shadow-5"
@@ -170,6 +170,7 @@
 // =========================================================================================================================================
 <script>
 import SectionHeader from "../components/SectionHeader.vue";
+const moment = require("moment");
 import { axios } from "boot/axios";
 
 export default {
@@ -181,8 +182,11 @@ export default {
       tab: "Dairy",
       date: "2021/03/18",
       list: undefined,
-      list2: undefined,
+      list5: undefined,
+      list_month: [],
       list_day: [],
+      list_select: [],
+      dashbord: [],
       text: "",
       columns: [
         {
@@ -233,31 +237,139 @@ export default {
     };
   },
   async mounted() {
-
-    let resp = await axios.get("http://mean.psu.ac.th:3000/api/visitors");
+    let resp = await axios.get("http://localhost:3030/api/visitors");
     this.list = resp.data.result.rows;
     console.warn("Visitor ");
     console.warn(resp.data.result.rows);
 
-    let resp2 = await axios.get("http://localhost:3030/api/history");
+    let resp2 = await axios.get("http://localhost:3030/api/taguse");
     this.list2 = resp2.data.result.rows;
-    // console.warn(resp.data.result.rows);
+    console.warn("list2 items taguse");
+    console.warn(this.list2);
 
-    for (var i = 0; i < 10; i++) {
-      var id = this.list2[i].id_visitor;
-      const newItem = {
-        id: id - 1,
-        name: this.list[id - 1].first_name + " " + this.list[id - 1].last_name,
-        date: this.list2[i].date,
-        time_start: this.list2[i].time_start,
-        time_stop: this.list2[i].time_stop,
-        type: this.list[id - 1].category,
-        location: this.list2[i].room,
-      };
-      this.list_day.push(newItem);
+    let resp3 = await axios.get("http://localhost:3030/api/locations");
+    this.list3 = resp3.data.result.rows;
+    console.warn("list3 location");
+    console.warn(this.list3);
+
+    let resp4 = await axios.get("http://localhost:3030/api/scanlog");
+    this.list4 = resp4.data.result.rows;
+    console.warn("list4 scanerlog");
+    console.warn(this.list4);
+
+    console.log(this.list[0].first_name);
+    for (var i = 0; i < this.list2.length; i++) {
+      for (var j = 0; j < this.list.length; j++) {
+        if (this.list2[i].visitor_id == this.list[j].visitor_id) {
+          for (var k = 0; k < this.list4.length; k++) {
+            if (
+              this.list2[i].tag_address == this.list4[k].device_address &&
+              moment(this.list2[i].time_stop).format("YYYY-MM-DD hh:mm") ==
+                moment(this.list4[k].scan_timestamp).format("YYYY-MM-DD hh:mm")
+            ) {
+              for (var l = 0; l < this.list3.length; l++) {
+                if (this.list4[k].scanner_id == this.list3[l].scanner_address) {
+                  const newItem = {
+                    id: this.list2[i].visitor_id,
+                    date: moment(this.list2[i].time_start).format("YYYY-MM-DD"),
+                    location: this.list3[l].room,
+                    name:
+                      this.list[j].first_name + " " + this.list[j].last_name,
+                    time_start: moment(this.list2[i].time_start).format(
+                      "hh:mm"
+                    ),
+                    time_stop: moment(this.list2[i].time_stop).format("hh:mm"),
+                    type: this.list[j].category,
+                  };
+                  this.dashbord.push(newItem);
+                }
+              }
+            }
+            if (
+              this.list2[i].tag_address == this.list4[k].device_address &&
+              this.list2[i].time_stop == null
+            ) {
+              for (var l = 0; l < this.list3.length; l++) {
+                if (this.list4[k].scanner_id == this.list3[l].scanner_address) {
+                  const newItem = {
+                    id: this.list2[i].visitor_id,
+                    date: moment(this.list2[i].time_start).format("YYYY-MM-DD"),
+                    location: this.list3[l].room,
+                    name:
+                      this.list[j].first_name + " " + this.list[j].last_name,
+                    time_start: moment(this.list2[i].time_start).format(
+                      "hh:mm"
+                    ),
+                    time_stop: "In Use",
+                    type: this.list[j].category,
+                  };
+                  this.dashbord.push(newItem);
+                }
+              }
+              break;
+            }
+          }
+        }
+      }
     }
-    console.warn("this day");
-    console.warn(this.list_day);
+    console.warn(this.dashbord);
+     for (var i = 0; i < this.dashbord.length; i++) {
+        if (moment(this.dashbord[i].date).format("YYYY-MM") == "2021-01") {
+          const newItems = {
+            id: this.dashbord[i].id,
+            date: this.dashbord[i].date,
+            location: this.dashbord[i].location,
+            name: this.dashbord[i].name,
+            time_start: this.dashbord[i].time_start,
+            time_stop: this.dashbord[i].time_stop,
+            type: this.dashbord[i].type,
+          };
+          this.list_month.push(newItems);
+        }
+        
+      }
+      console.warn(this.list_month);
+       for (var i = 0; i < this.dashbord.length; i++) {
+        if (moment(this.dashbord[i].date).format("YYYY-MM-DD") == "2021-02-13") {
+          const newItems = {
+            id: this.dashbord[i].id,
+            date: this.dashbord[i].date,
+            location: this.dashbord[i].location,
+            name: this.dashbord[i].name,
+            time_start: this.dashbord[i].time_start,
+            time_stop: this.dashbord[i].time_stop,
+            type: this.dashbord[i].type,
+          };
+          this.list_day.push(newItems);
+        }
+        
+      }
+      console.warn(this.list_day);
+       this.list_select = this.dashbord
+      console.warn(this.list_select);
+      
+  },
+  methods: {
+    async select() {
+      console.warn(this.date)
+     this.list_select = []
+     for (var i = 0; i < this.dashbord.length; i++) {
+        if (moment(this.dashbord[i].date).format("YYYY/MM/DD") == this.date) {
+          const newItems = {
+            id: this.dashbord[i].id,
+            date: this.dashbord[i].date,
+            location: this.dashbord[i].location,
+            name: this.dashbord[i].name,
+            time_start: this.dashbord[i].time_start,
+            time_stop: this.dashbord[i].time_stop,
+            type: this.dashbord[i].type,
+          };
+          this.list_select.push(newItems);
+        }
+        
+      }
+      console.warn(this.list_select);
+    },
   },
 };
 </script>
