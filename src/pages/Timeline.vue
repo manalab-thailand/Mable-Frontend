edf7e39a69c1f3f673711610f0c40feba580a318
Rusfirdao <60874008+Rusfirdao@users.noreply.github.com>
2021-08-13 16:58:13 +0700

    Update Timeline.vue

--- src/pages/Timeline.vue
+++ src/pages/Timeline.vue
@@ -1,161 +1,166 @@
 <template>
-  <div>
-    <div class="grid-container">
-      <div class="info-zone">
-        <p>Name: {{}}</p>
-        <p>Tel: {{}}</p>
-        <p>Type: {{}}</p>
-        <p>Location: {{}}</p>
-      </div>
-      <q-card class="my-card">
-        <q-card-section class="card-zone">
-          <div class="icon-zone">
-            <i class="fas fa-file-alt"></i>
-          </div>
-          <div class="tag-zone">Tag {{}}</div>
-          <q-btn-dropdown
-            class="more-detail"
-            color="white"
-            text-color="primary"
-            label="..."
-            size="sm"
+  <q-page class="q-pa-sm">
+    <section-header
+      title="Time Line"
+      subTitle="แสดงข้อมูลการใช้งานของ User"
+    ></section-header>
+    <!-------------------------- Infomation User ----------------------------------->
+  <q-layout view="hHh Lpr lFf">
+      <q-page-container class="bg-grey-2">
+              <div v-for="itemz in list" :key="itemz.visitor_id" >
+                <div v-if="itemz.visitor_id == id" class="row justify-center" >
+                
+                  <div class="col-md-8 col-xs-12 q-pa-md " >
+                    <info-card
+                      class="col-4"
+                      :fname="itemz.first_name"
+                      :lname="itemz.last_name"
+                      :tel="itemz.tel"
+                      :id_civiliz="itemz.id_civiliz"
+                      :category="itemz.category"
+                      :contract="itemz.contract"
+                    />
+                  </div>         
+                    <div class="col-md-3 col-xs-12 q-pa-md text-blue-8" >
+                        <q-card
+                          class="col-3 rounded-borders-10 shadow-5 text-center"
+                          id="my-card-2" 
+                        >
+                          <q-img height="auto" width="150px" 
+                            placeholder-src="~assets/timeline.png"
+                            src="~assets/timeline.png"
+                            spinner-color="white"
+                          ></q-img>
+
+                          <q-card-section class="text-h5 text-center">
+                            <div>Tag {{ itemz.tag_id }}</div>
+                          </q-card-section>
+                        </q-card>
+                  
+                    </div>
+              
+                </div>
+              </div>
+       
+        <!--------------------------- Timeline User ----------------------------------->
+        <q-timeline color="secondary" style="padding-left: 4rem " >
+          <q-timeline-entry heading> Timeline </q-timeline-entry>
+
+          <q-timeline-entry
+            v-for="time in timeline"
+            :key="time.time"
+            :title="'Room : ' + time[time.length - 2]"
+            :subtitle="time[time.length - 1]"
           >
-            <q-list dense>
-              <q-item tag="label" to="/timeline">
-                <q-item-section>Timeline</q-item-section>
-              </q-item>
-            </q-list>
-          </q-btn-dropdown>
-        </q-card-section>
-      </q-card>
-    </div>
-    <q-timeline color="secondary" style="padding:2rem;">
-      <q-timeline-entry heading>
-        Timeline heading
-      </q-timeline-entry>
-
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 21, 1986"
-        icon="delete"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry heading>
-        November, 2017
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        avatar="https://cdn.quasar.dev/img/avatar2.jpg"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        color="orange"
-        icon="done_all"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-    </q-timeline>
-  </div>
+            Time : {{ time[time.length - 3] }} - {{ time[0] }}
+          </q-timeline-entry>
+          <q-timeline-entry title=" Start " subtitle=" " icon="watch_later">
+          </q-timeline-entry>
+        </q-timeline>
+        <!---------------------------close Timeline User ----------------------------------->
+      </q-page-container>
+   </q-layout>
+  </q-page>
 </template>
 
 <script>
-export default {}
+import SectionHeader from "../components/SectionHeader.vue";
+import InfoCard from "../components/InfoCard.vue";
+import { axios } from "boot/axios";
+const moment = require("moment");
+export default {
+  components: {
+    SectionHeader,
+    InfoCard,
+  },
+
+  data() {
+    return {
+      id: this.$route.params.id,
+      timeline: [],
+      time: [],
+      list: undefined,
+      list2: undefined,
+      tag: undefined,
+    };
+  },
+  async mounted() {
+    setTimeout(function () {
+      location.reload(1);
+    }, 60000);
+    //<------------------------- Connect Database ---------------------------------------------------หฟกฟหกฟหก----->
+    // const url = "http://localhost:3030/api/";
+    const url = "http://mean.psu.ac.th:3000/api/";
+    let resp1 = await axios.get(url + "visitors");
+    this.list = resp1.data.result.rows;
+    console.warn("list item visitors");
+    console.warn(this.list);
+
+    for (var j = 0; j < this.list.length; j++) {
+      if (this.id == this.list[j].visitor_id) {
+        break;
+      }
+    }
+    console.warn(" j : " + j);
+    let resp2 = await axios.get(url + "selectlog", {
+      params: {
+        device_address: this.list[j].tag_address,
+        time_start: moment(this.list[j].time_start).format(
+          "YYYY-MM-DD hh:mm:ss A"
+        ),
+        time_stop: moment(this.list[j].time_stop)
+          .add(1, "minute")
+          .format("YYYY-MM-DD hh:mm:ss A"),
+      },
+    });
+    this.list2 = resp2.data.result.rows;
+    console.warn("list2 scanerlog");
+    console.warn(this.list2);
+
+    //<------------------------- Create Timeline ----------------------------------->
+    var rooms = this.list2[0].room;
+    var times = moment(this.list2[0].scan_timestamp).format("hh:mm A");
+    console.warn(moment(this.list2[0].scan_timestamp).format("hh:mm A"));
+    if (this.list[j].time_stop == null) {
+      this.time.push("In Use Now.");
+    }
+    for (var i = 0; i < this.list2.length; i++) {
+      if (
+        this.list2[i].room != rooms
+        // && moment(this.list2[i].scan_timestamp).format("hh:mm A") != times
+      ) {
+        this.time.push(this.list2[i - 1].room);
+        this.time.push(
+          moment(this.list2[i].scan_timestamp).format("YYYY-MM-DD")
+        );
+        this.timeline.push(this.time);
+        this.time = [];
+        rooms = this.list2[i].room;
+        times = moment(this.list2[i].scan_timestamp).format("hh:mm A");
+        --i;
+      } else {
+        this.time.push(moment(this.list2[i].scan_timestamp).format("hh:mm A"));
+      }
+    }
+
+    this.time.push(this.list2[this.list2.length - 1].room);
+    this.time.push(
+      moment(this.list2[this.list2.length - 1].scan_timestamp).format(
+        "YYYY-MM-DD"
+      )
+    );
+
+    this.timeline.push(this.time);
+    console.warn(this.timeline);
+  },
+};
 </script>
 
 <style lang="sass" scoped>
-.grid-container
-    display: flex
-    padding: 2rem
-    width: 100%
-.card-zone
-    background-color: blue
-    text-align: center
-.info-zone
-    background-color: lightgray
-    justify-content: center
-    width: 50%
-.my-card
-    width: 50%
-    max-width: 300px
-    margin: 2rem
+#my-card-1
+  padding: 27px
+#my-card-2
+  padding: 10px
+#img
+  display: inline-block 
 </style>
